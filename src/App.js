0cc304fb594d67faacda25ddebbb4b38217d9b7d
Saif <saifchogle@gmail.com>
2020-11-05 10:56:56 +0530

    Working firebase connection

--- src/App.js
+++ src/App.js
@@ -1,26 +1,47 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import * as firebase from 'firebase';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+
+
+class App extends React.Component {
+  constructor(){
+    super();
+    this.state = {
+      Distance:0
+    };
+
+  }
+
+  componentDidMount() {
+   
+    const rootRef = firebase.database().ref().child('Distance');
+    const distRef = rootRef.child('Distance');
+    rootRef.on('value', snap => {
+      this.setState({
+        Distance: snap.val()
+      
+      });
+      
+    });
+    
+  }
+
+  render(){
+    return(
+     
+      <div className="App">
+      
+        <h1> RealTime Distance: {this.state.Distance}cm</h1>
+ 
+      </div>
+
+    
+  
+    )
+}
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -3,10 +3,27 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import * as firebase from 'firebase';
+
+
+var firebaseConfig = {
+  apiKey: "AIzaSyDcoaTJr1K6PfTy8ylKI_9-m4j7VtPH8t8",
+  authDomain: "nodemce.firebaseapp.com",
+  databaseURL: "https://nodemce.firebaseio.com",
+  projectId: "nodemce",
+  storageBucket: "nodemce.appspot.com",
+  messagingSenderId: "284670480170",
+  appId: "1:284670480170:web:0b9a03e6f635b4cbcf9853",
+  measurementId: "G-G7STTWM1NK"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+firebase.analytics();
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
+   
   </React.StrictMode>,
   document.getElementById('root')
 );
